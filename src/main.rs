481981f0944320a8ee7d2449491cbef15997a3b4
stinta <kyrbynyt@gmail.com>
2025-02-06 22:02:15 +0200

    huhu yo

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,5 @@ use lalrpop_util::lalrpop_mod;
 lalrpop_mod!(grammar);
 
 fn main() {
-    dbg!(grammar::ExpressionParser::new().parse("123 + 9"));
+    dbg!(grammar::ExpressionParser::new().parse("123 + 9").unwrap());
 }
