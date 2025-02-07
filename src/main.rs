125073b8ddd8acabd938028f75d9d05969c98514
stinta <kyrbynyt@gmail.com>
2025-02-07 18:02:11 +0200

    Minor refactor

--- src/main.rs
+++ src/main.rs
@@ -12,25 +12,20 @@ lalrpop_mod!(grammar);
 fn main() {
     let config = cli_config::Config::parse();
 
-    match config.mode() {
-        cli_config::Mode::Repl => repl::repl(),
-        cli_config::Mode::Python => {
-            let input = std::fs::read_to_string(config.input().unwrap()).unwrap();
+    match (config.mode(), config.input(), config.output()) {
+        (cli_config::Mode::Repl, _, _) => repl::repl(),
+        (cli_config::Mode::Python, Some(input), Some(output)) => {
+            let input = std::fs::read_to_string(input).unwrap();
             let program = grammar::ProgramParser::new().parse(&input).unwrap();
-            std::fs::write(
-                config.output().unwrap(),
-                python::transpile_to_python(&program).unwrap(),
-            )
-            .unwrap()
+            std::fs::write(output, python::transpile_to_python(&program).unwrap()).unwrap()
         }
-        cli_config::Mode::Interpret => {
-            let program = grammar::ProgramParser::new()
-                .parse(&config.input().unwrap())
-                .unwrap();
+        (cli_config::Mode::Interpret, Some(input), _) => {
+            let program = grammar::ProgramParser::new().parse(input).unwrap();
             match interpreter::interpret(&program) {
                 Ok(exit_code) => println!("Program quit succesfully with: {exit_code}"),
                 Err(e) => println!("An error occured during interpretation: {e:?}"),
             }
         }
+        _ => unreachable!(),
     }
 }
